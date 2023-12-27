98a6a88d3915793d5e64a0bcff15ef981f9dd4cd
CrimsonDemon <67166086+NachABR@users.noreply.github.com>
2023-12-26 22:55:24 -0300

    Fix fzf.zsh

--- zsh/.fzf.zsh
+++ zsh/.fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/crimson/.fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/home/crimson/.fzf/bin"
+if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/crimson/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/crimson/.fzf/shell/key-bindings.zsh"
+source "$HOME/.fzf/shell/key-bindings.zsh"
